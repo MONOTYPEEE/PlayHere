51c26ad89a822d79c6e86b441827f6756a50519f
김상구 <spair0039@gmail.com>
2023-05-18 20:23:15 +0900

    config recoil, fluent

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import '@/styles/globals.css'
+import { FluentProvider, webLightTheme } from '@fluentui/react-components'
 import type { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil'
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <RecoilRoot>
-        <Component {...pageProps} />
-        </RecoilRoot>
+        <FluentProvider theme={webLightTheme}>
+            <RecoilRoot>
+                <Component {...pageProps} />
+            </RecoilRoot>
+        </FluentProvider>
     )
 }
