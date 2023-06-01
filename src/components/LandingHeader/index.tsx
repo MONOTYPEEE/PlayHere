f4cf38aa59c965c041906cea532c1dc406d9aa64
김상구 <spair0039@gmail.com>
2023-06-01 16:48:18 +0900

    ✨ 탭 이동 구현

--- src/components/LandingHeader/index.tsx
+++ src/components/LandingHeader/index.tsx
@@ -1,3 +1,5 @@
+import { LandingTabState } from "@/atoms/LandingAtom";
+import { useRecoilState } from "recoil";
 import { Card } from "@fluentui/react-card";
 import { Button, Tab, TabList, makeStyles } from "@fluentui/react-components";
 import { Home24Filled, Home24Regular, bundleIcon } from "@fluentui/react-icons";
@@ -5,12 +7,13 @@ import { Home24Filled, Home24Regular, bundleIcon } from "@fluentui/react-icons";
 const Home24Bundle = bundleIcon(Home24Filled, Home24Regular);
 
 export default function LandingHeader() {
-    const style = Center()
+    const style = Center();
+    const [TabState, setTabState] = useRecoilState<string>(LandingTabState);
 
     return(
         <div className={style.root}>
             <Card appearance="filled" size="small" focusMode="off" role="navigation" orientation="horizontal">
-                <TabList appearance="subtle" size="small" >
+                <TabList appearance="subtle" size="small" defaultSelectedValue="1" selectedValue={TabState} onTabSelect={(e,v)=>(setTabState(v.value))}>
                     <Tab value="1" icon={<Home24Bundle/>} />
                     <Tab value="2">서비스 소개</Tab>
                     <Tab value="3">공지사항</Tab>
