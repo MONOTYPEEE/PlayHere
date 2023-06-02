4229ed79861f43c981b8ed3d24fe5d02b8239023
김상구 <spair0039@gmail.com>
2023-06-02 15:23:11 +0900

    🎨 캐치프레이즈 추가, flex 배치

--- src/components/LandingHomePage/index.tsx
+++ src/components/LandingHomePage/index.tsx
@@ -1,8 +1,30 @@
-import { Display } from "@fluentui/react-components";
+import { Body1, Display, makeStyles, tokens } from "@fluentui/react-components";
 import HomeLoginComponent from "../HomeLoginComponent";
 
 export default function LandingHomePage(){
+    const style = Style();
+
     return(
-        <HomeLoginComponent/>
+        <div className={style.Section1}>
+            <HomeLoginComponent/>
+            <div className={style.Section1Inner}>
+                <Display>우리, 여기서<br/>플레이!</Display>
+                <Body1>PlayHere는 어쩌구 소개문구입니다<br/>와 br도 했어요</Body1>
+            </div>
+        </div>
     )
 }
+
+const Style = makeStyles({
+        Section1:{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            columnGap: tokens.spacingHorizontalXXXL,
+            height: '100vh'
+        },
+        Section1Inner:{
+            display:'flex',
+            flexDirection:'column'
+        }
+    })
