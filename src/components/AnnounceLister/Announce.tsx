e25a12f377457e0677b92d019f9982f9fe6e202c
김상구 <spair0039@gmail.com>
2023-06-07 00:39:43 +0900

    ♿ Image Alt 추가

--- src/components/AnnounceLister/Announce.tsx
+++ src/components/AnnounceLister/Announce.tsx
@@ -14,7 +14,7 @@ export default function AnnounceComponent({postID, thumbnailURL, title, shortDes
 
     return(
         <div className={Style.root}>
-            <Image src={thumbnailURL} />
+            <Image src={thumbnailURL} alt="커버 이미지"/>
             <Title3>{title}</Title3>
             <Body1>{shortDesc}</Body1>
         </div>
