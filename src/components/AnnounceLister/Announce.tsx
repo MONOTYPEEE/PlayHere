2b1fe3fe57e206034420f2930ba5c280e3c825c2
김상구 <spair0039@gmail.com>
2023-06-14 22:14:04 +0900

    🎨 AnnounceComponent div -> Card 전환

--- src/components/AnnounceLister/Announce.tsx
+++ src/components/AnnounceLister/Announce.tsx
@@ -1,4 +1,4 @@
-import { Body1, Image, Title3 } from "@fluentui/react-components";
+import { Body1, Card, Image, Title3 } from "@fluentui/react-components";
 import { type } from "os";
 import { AnnounceStyle } from "./style";
 
@@ -13,10 +13,10 @@ export default function AnnounceComponent({postID, thumbnailURL, title, shortDes
     const Style = AnnounceStyle();
 
     return(
-        <div className={Style.root}>
+        <Card appearance="subtle">
             <Image src={thumbnailURL} alt="커버 이미지"/>
             <Title3>{title}</Title3>
             <Body1>{shortDesc}</Body1>
-        </div>
+        </Card>
     )
 }
