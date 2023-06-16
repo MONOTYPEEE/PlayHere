d0c7bc887dbbccbbe977b3218e52b5a2ccdabbfb
김상구 <spair0039@gmail.com>
2023-06-16 12:16:17 +0900

    🐛 env 수정

--- src/lib/supabaseInit.tsx
+++ src/lib/supabaseInit.tsx
@@ -1,3 +1,3 @@
 import { createClient } from "@supabase/supabase-js"
 
-export const supabase = createClient(process.env.SB_URL as string, process.env.SB_KEY as string);
+export const supabase = createClient(process.env.NEXT_PUBLIC_SB_URL as string, process.env.NEXT_PUBLIC_SB_APIKEY as string);
