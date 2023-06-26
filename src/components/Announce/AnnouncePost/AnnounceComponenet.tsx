55d0cfe7a152261440d117cb7778621c673a2961
김상구 <spair0039@gmail.com>
2023-06-26 16:19:32 +0900

    ✨ AnnounceComponent 타입 수정, 날짜 추가

--- src/components/Announce/AnnouncePost/AnnounceComponenet.tsx
+++ src/components/Announce/AnnouncePost/AnnounceComponenet.tsx
@@ -1,23 +1,17 @@
-import { Body1, Card, Image, Title3 } from "@fluentui/react-components";
-import { type } from "os";
+import { Body1, Card, Image, Subtitle1, Title3 } from "@fluentui/react-components";
 import { AnnounceStyle } from "./style";
 import useNoticeThumb from "@/hooks/useNoticeThumb";
 
-type AnnounceType = {
-    postID: string,
-    title: string,
-    shortDesc: string,
-}
-
-export default function AnnounceComponent({postID, title, shortDesc}:AnnounceType){
+export default function AnnounceComponent({id, title, shortDesc, Created}:Notice){
     const Style = AnnounceStyle();
-    const thumbURL = useNoticeThumb(postID);
+    const thumbURL = useNoticeThumb(id);
 
     return(
         <Card appearance="subtle">
             <Image src={thumbURL} alt="커버 이미지" shape="rounded" shadow/>
             <Title3>{title}</Title3>
-            <Body1>{shortDesc}</Body1>
+            <Subtitle1>{shortDesc}</Subtitle1>
+            <Body1>{Created}</Body1>
         </Card>
     )
 }
