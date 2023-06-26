435c2dcb815c099017afff8b912db843ac1d4900
김상구 <spair0039@gmail.com>
2023-06-26 16:20:02 +0900

    ✨ AnnouncePage 데이터 출력

--- src/components/Announce/AnnouncePage/index.tsx
+++ src/components/Announce/AnnouncePage/index.tsx
@@ -1,15 +1,19 @@
 import { Title1 } from "@fluentui/react-components";
 import { AnnouncementStyle } from "./style";
-import AnnounceComponent from "../AnnouncePost/Announce";
+import AnnounceComponent from "../AnnouncePost/AnnounceComponenet";
+import useNotice from "@/hooks/useNotice";
 
 export default function AnnouncePage(){
     const Style = AnnouncementStyle();
+    const data = useNotice();
 
     return(
         <div className={Style.root}>
             <div className={Style.clild}>
                 <Title1>공지사항</Title1>
-                <AnnounceComponent postID={123} shortDesc="더 안전해진 보안과 새로운 기술을 적용한 R323 업데이트를 소개합니다" thumbnailURL="https://grnaqmvtqsslvrivlqob.supabase.co/storage/v1/object/public/blog/92c491e6-4824-4672-9090-2cea03b212b7_1x.webp" title="R232 업데이트 노트" key={124}/>
+                {data.map((d)=>{
+                    return <AnnounceComponent id={d.id} shortDesc={d.shortDesc} title={d.title} Created={d.Created} key={d.id} body={d.body}/>
+                })}
             </div>
         </div>
     )
