ad4e2b4fcbec7e6832f7f78e1ef39056b9216a5e
김상구 <spair0039@gmail.com>
2023-06-26 17:04:49 +0900

    ✨ useNotice 컨셉 변경

--- src/hooks/useNoticeList.tsx
+++ src/hooks/useNoticeList.tsx
@@ -1,18 +1,18 @@
 import { supabase } from "@/lib/supabaseInit";
 import { useState, useEffect } from "react";
 
-export default function useNotice() {
+export default function useNoticeList() {
     const [storage, setStorage] = useState<Notice[]>([]);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const { data, error } = await supabase.from('notice').select();
+                const { data, error } = await supabase.from('Blog').select('Created,id,title,shortDesc');
                 if (!error && data) {
                     setStorage(data as Notice[]);
                 }
             } catch (error) {
-                console.error('Error@useNotice');
+                console.error('Error@useNoticeList');
             }
         };
 
