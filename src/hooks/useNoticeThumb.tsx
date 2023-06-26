308714f4513b2dd396063ca71e8f5720dda48bd0
김상구 <spair0039@gmail.com>
2023-06-26 16:13:17 +0900

    🐛 useNoticeThumb 타입 수정

--- src/hooks/useNoticeThumb.tsx
+++ src/hooks/useNoticeThumb.tsx
@@ -1,7 +1,3 @@
-interface useNoticeThumbType{
-    id: string;
-}
-
-export default function useNoticeThumb(id:useNoticeThumbType){
+export default function useNoticeThumb(id:string){
     return process.env.NEXT_PUBLIC_SB_URL + '/storage/v1/object/public/blog/' + id + '.webp'
 }
