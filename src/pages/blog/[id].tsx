be47aaf696cc8e51277db0dd25043408ddfdac25
김상구 <spair0039@gmail.com>
2023-07-07 18:56:54 +0900

    🎨 블로그 글 페이지 여백 적용

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -1,12 +1,20 @@
+import { BlogPageStyle } from "@/components/Blog/BlogPage/style";
 import LandingHeader from "@/components/Landing/LandingHeader";
 import { useRouter } from "next/router"
 
 export default function BlogPost(){
     const r = useRouter();
+    const style = BlogPageStyle();
 
     return(
         <>
             <LandingHeader/>
+            <div className={style.root}>
+                <div className={style.clild}>
+                    {r.query.id}
+                </div>
+            </div>
+            
         </>
     )
 }
