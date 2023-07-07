cf4cf7321f2f201be147b809273ef8f3ddfc9849
김상구 <spair0039@gmail.com>
2023-07-07 18:28:15 +0900

    ✨ 썸네일 이름 규칙 반영

--- src/hooks/useBlogThumb.tsx
+++ src/hooks/useBlogThumb.tsx
@@ -1,3 +1,3 @@
 export default function useBlogThumb(id:string){
-    return process.env.NEXT_PUBLIC_SB_URL + '/storage/v1/object/public/blog/' + id + '.webp'
+    return process.env.NEXT_PUBLIC_SB_URL + '/storage/v1/object/public/blog/' + id + '_cover.webp'
 }
