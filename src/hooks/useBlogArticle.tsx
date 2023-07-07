edea292754f755f5ace1eb4dcfdeb1807393b19b
김상구 <spair0039@gmail.com>
2023-07-07 19:44:52 +0900

    ✨ useBlogArticle hook

---
+++ src/hooks/useBlogArticle.tsx
@@ -0,0 +1,24 @@
+import { supabase } from "@/lib/supabaseInit";
+import { useState, useEffect } from "react";
+
+export default function useBlogArticle(postId: string) {
+    const [storage, setStorage] = useState<BlogPost>();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const { data, error } = await supabase.from('Blog').select('created,title,shortDesc,body').eq('id',postId).single();
+                if (!error && data) {
+                    setStorage(data);
+                    console.log(data);
+                }
+            } catch (error) {
+                console.error('Error@useBlogList');
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return storage;
+}
