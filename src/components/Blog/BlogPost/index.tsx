7730f1d231212fa22ef55c9a15ff5d7535d7f600
김상구 <spair0039@gmail.com>
2023-07-07 18:31:07 +0900

    ✨ 블로그 클릭시 이동

--- src/components/Blog/BlogPost/index.tsx
+++ src/components/Blog/BlogPost/index.tsx
@@ -1,13 +1,19 @@
 import { Body1, Card, Image, Subtitle1, Title3 } from "@fluentui/react-components";
 import { BlogPostStyle } from "./style";
 import useBlogThumb from "@/hooks/useBlogThumb";
+import { useRouter } from "next/router";
 
 export default function BlogPost({id, title, shortDesc, Created}:BlogPost){
     const Style = BlogPostStyle();
     const thumbURL = useBlogThumb(id);
+    const r = useRouter();
+    
+    function openArticle(){
+        r.push(`/blog/${id}`);
+    }
 
     return(
-        <Card appearance="subtle" className={Style.root}>
+        <Card appearance="subtle" className={Style.root} onClick={openArticle}>
             <Image src={thumbURL} alt="커버 이미지" shape="rounded" shadow/>
             <Title3>{title}</Title3>
             <Subtitle1>{shortDesc}</Subtitle1>
