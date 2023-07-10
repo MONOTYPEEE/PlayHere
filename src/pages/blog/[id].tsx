f84792bfe0747729130f5370454dfd03c7a20ca6
김상구 <spair0039@gmail.com>
2023-07-10 14:53:12 +0900

    ✨ 본문 이미지 및 제목, 부제목 로드

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -1,20 +1,30 @@
 import { BlogPageStyle } from "@/components/Blog/BlogPage/style";
 import LandingHeader from "@/components/Landing/LandingHeader";
+import useBlogArticle from "@/hooks/useBlogArticle";
+import useBlogThumb from "@/hooks/useBlogThumb";
+import { Image, LargeTitle, Subtitle1, Subtitle2 } from "@fluentui/react-components";
 import { useRouter } from "next/router"
 
 export default function BlogPost(){
     const r = useRouter();
+    const articleId = r.query.id as string;
+
     const style = BlogPageStyle();
+    const data = useBlogArticle(articleId);
+
+    const thumb = useBlogThumb(articleId);
 
     return(
         <>
             <LandingHeader/>
             <div className={style.root}>
                 <div className={style.clild}>
-                    {r.query.id}
+                    <Image src={thumb} />
+                    <LargeTitle>{data?.title}</LargeTitle>
+                    <Subtitle1>{data?.shortDesc}</Subtitle1>
+                    <Subtitle2>{data?.Created}</Subtitle2>
                 </div>
             </div>
-            
         </>
     )
 }
