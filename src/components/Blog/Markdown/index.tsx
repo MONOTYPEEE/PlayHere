c673b75b5761324f42b410de74e38ee650e7f4b2
김상구 <spair0039@gmail.com>
2023-07-10 20:21:46 +0900

    🎨 Markdown 컴포넌트 스타일링

--- src/components/Blog/Markdown/index.tsx
+++ src/components/Blog/Markdown/index.tsx
@@ -1,21 +1,26 @@
-import { Title1, Title2, Title3, makeStyles } from "@fluentui/react-components";
+import { Body2, Title1, Title2, Title3, makeStyles } from "@fluentui/react-components";
 import { HeadingComponent } from "react-markdown/lib/ast-to-react";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import { MarkdownStyle } from "./style";
+import React from "react";
 
 interface MarkdownType{
     body: string;
 }
 
 export default function Markdown({body}:MarkdownType){
-
+    const style = MarkdownStyle();
 
     return(
-        <ReactMarkdown components={{
-            h1: Title1 as HeadingComponent,
-            h2: Title2 as HeadingComponent,
-            h3: Title3 as HeadingComponent,
-        }}>
-            {body}
-        </ReactMarkdown>
+        <div className={style.body}>
+            <ReactMarkdown components={{
+                h1: Title1 as HeadingComponent,
+                h2: Title2 as HeadingComponent,
+                h3: Title3 as HeadingComponent,
+                p: Body2,
+            }}>
+                {body}
+            </ReactMarkdown>
+        </div>
     )
 }
