7a2e962bc5512da57705d5c2b4bf689455476397
김상구 <spair0039@gmail.com>
2023-07-12 09:29:19 +0900

    🎨 BlogPost 컴포넌트 TimeDiffBlock 전환

--- src/components/Blog/BlogPost/index.tsx
+++ src/components/Blog/BlogPost/index.tsx
@@ -1,7 +1,8 @@
-import { Body1, Card, Image, Subtitle1, Title3 } from "@fluentui/react-components";
+import { Body1, Card, Image, Subtitle1, Subtitle2, Title3 } from "@fluentui/react-components";
 import { BlogPostStyle } from "./style";
 import useBlogThumb from "@/hooks/useBlogThumb";
 import { useRouter } from "next/router";
+import TimeDiffBlock from "../TimeDiffBlock";
 
 export default function BlogPost({id, title, shortDesc, Created}:BlogPost){
     const Style = BlogPostStyle();
@@ -17,7 +18,7 @@ export default function BlogPost({id, title, shortDesc, Created}:BlogPost){
             <Image src={thumbURL} alt="커버 이미지" shape="rounded" shadow/>
             <Title3>{title}</Title3>
             <Subtitle1>{shortDesc}</Subtitle1>
-            <Body1>{Created}</Body1>
+            <TimeDiffBlock as={Subtitle2} time={Created} transparent/>
         </Card>
     )
 }
