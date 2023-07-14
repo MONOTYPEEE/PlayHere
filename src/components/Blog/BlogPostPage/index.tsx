ebd563e281bb0a3314ba73d9647688dee18ca8cf
김상구 <spair0039@gmail.com>
2023-07-14 10:15:19 +0900

    ✨ ArticleId useEffect 추가

--- src/components/Blog/BlogPostPage/index.tsx
+++ src/components/Blog/BlogPostPage/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import useBlogThumb from "@/hooks/useBlogThumb";
 import useBlogArticle from "@/hooks/useBlogArticle";
 import { BlogArticlePageStyle } from "./style";
@@ -8,19 +9,26 @@ import TimeDiffBlock from "../TimeDiffBlock";
 
 export default function BlogPostPage(){
     const r = useRouter();
-    const articleId = r.query.id as string;
+    const [PageId,setPageId] = useState<string>('');
     const style = BlogArticlePageStyle();
-    const data = useBlogArticle(articleId);
-    const thumb = useBlogThumb(articleId);
+    const ArticleData = useBlogArticle(PageId);
+    const thumb = useBlogThumb(PageId);
+
+    useEffect(()=>{
+        if(r.isReady){
+            console.log(r)
+            setPageId(r.query.id as string);
+        }
+    },[r.query.id, r.isReady])
 
     return(
         <div className={style.clild}>
             <Image src={thumb} shadow shape="rounded"/>
-            <LargeTitle>{data.title}</LargeTitle>
-            <Subtitle1 className={style.transparent}>{data.shortDesc}</Subtitle1>
-            <TimeDiffBlock as={Subtitle1} time={data.created} transparent/>
+            <LargeTitle>{ArticleData.title}</LargeTitle>
+            <Subtitle1 className={style.transparent}>{ArticleData.shortDesc}</Subtitle1>
+            <TimeDiffBlock as={Subtitle1} time={ArticleData.created} transparent/>
             <Divider/>
-            <Markdown body={data.body}/>
+            <Markdown body={ArticleData.body}/>
         </div>
     )
 }
