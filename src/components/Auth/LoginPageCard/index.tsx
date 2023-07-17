4094254a57d3a80019e2f0c21de05fe6bc3b4485
김상구 <spair0039@gmail.com>
2023-07-17 13:05:10 +0900

    🎨 LoginPageCard 기본 구조 작성

---
+++ src/components/Auth/LoginPageCard/index.tsx
@@ -0,0 +1,30 @@
+import { Card, Label, Title2, Input } from "@fluentui/react-components";
+import { useState } from "react";
+
+export default function LoginPageCard(){
+    const [FormData,setFormData] = useState({id:'',pw:''});
+
+    function ValueChange(e){
+        const { name, value } = e.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    }
+
+    return(
+        <Card>
+            <Title2>로그인</Title2>
+            <form>
+                <Label size="large">
+                    아이디
+                    <Input size="large" name="id" value={FormData.id} onChange={ValueChange}/>
+                </Label>
+                <Label size="large">
+                    비밀번호
+                    <Input size="large" name="pw" value={FormData.pw} onChange={ValueChange} type="password"/>
+                </Label>
+            </form>
+        </Card>
+    )
+}
