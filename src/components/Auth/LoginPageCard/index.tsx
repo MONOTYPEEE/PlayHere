80bb3236f319fb853fe65dd71a4eef1c3d3f1612
김상구 <spair0039@gmail.com>
2023-07-20 16:51:38 +0900

    ♻️ 아 콘솔로그 안지웠따

--- src/components/Auth/LoginPageCard/index.tsx
+++ src/components/Auth/LoginPageCard/index.tsx
@@ -46,7 +46,6 @@ export default function LoginPageCard(){
                     if(error) setPasswordError(error?.message);
                 })
             supabase.auth.onAuthStateChange((event, session) => {
-                console.log(event, session)
                 if(event==='SIGNED_IN'){
                     changeDir('/');
                 }
