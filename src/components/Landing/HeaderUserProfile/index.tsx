f65425a86f5866401cfc6259d8f68ba1fc0a204d
김상구 <spair0039@gmail.com>
2023-07-21 16:04:25 +0900

    ✨ 로그아웃 기능구현

--- src/components/Landing/HeaderUserProfile/index.tsx
+++ src/components/Landing/HeaderUserProfile/index.tsx
@@ -1,13 +1,28 @@
+import { IsLoggedinState } from "@/atoms/LoginAtom";
 import { supabase } from "@/lib/supabaseInit";
 import { MenuButton, Menu, MenuItem, MenuList, MenuPopover, MenuTrigger, Persona } from "@fluentui/react-components";
 import { SignOut24Filled } from "@fluentui/react-icons";
 import { Session } from "@supabase/supabase-js";
+import { useRouter } from "next/router";
+import { useRecoilState } from "recoil";
 
 interface HeaderUserProfileType{
     data: Session | null | undefined;
 }
 
 export default function HeaderUserProfile({data}:HeaderUserProfileType){
+    const router = useRouter();
+    const [isLoggedIn,setLoggedIn] = useRecoilState(IsLoggedinState);
+
+    function signOutHandler(){
+        supabase.auth.signOut();
+        supabase.auth.onAuthStateChange((event, session)=>{
+            if(event === 'SIGNED_OUT'){
+                setLoggedIn(false);
+            }
+        })
+    }
+
     return(
         <Menu>
             <MenuTrigger>
@@ -18,7 +33,7 @@ export default function HeaderUserProfile({data}:HeaderUserProfileType){
 
             <MenuPopover>
                 <MenuList>
-                    <MenuItem icon={<SignOut24Filled/>}>로그아웃</MenuItem>
+                    <MenuItem icon={<SignOut24Filled/>} onClick={signOutHandler}>로그아웃</MenuItem>
                 </MenuList>
             </MenuPopover>
         </Menu>
