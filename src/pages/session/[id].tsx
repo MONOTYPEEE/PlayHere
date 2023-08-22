55638ba7213a5a0644d62bd862f7583e096d41ba
김상구 <spair0039@gmail.com>
2023-08-22 19:01:01 +0900

    ✨ 작동 테스트용 코드

--- src/pages/session/[id].tsx
+++ src/pages/session/[id].tsx
@@ -1,19 +1,42 @@
-import { useRouter } from "next/router";
+import { supabase } from "@/lib/supabaseInit"
+import { RealtimeChannel } from "@supabase/supabase-js"
+import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 
 export default function InsideSession(){
-    const router = useRouter();
-    const [SessionID,setSessionID] = useState<string>('');
+    const router = useRouter()
+    const [SessionID,setSessionID] = useState<string>('')
+    const [LiveChannel, setLiveChannel] = useState<RealtimeChannel>()
 
     useEffect(()=>{
         if(router.isReady){
             setSessionID(router.query.id as string)
         }
     },[router.isReady])
+    useEffect(()=>{
+        if(SessionID){
+            setLiveChannel(
+                supabase.channel(SessionID).subscribe()
+            )
+            console.log(LiveChannel)
+            LiveChannel?.on('broadcast', {event:'msg'},payload=>{
+                console.log(payload.payload.payload)
+            })
+        }
+    },[SessionID])
+
+    function sendmsg(){
+        LiveChannel?.send({
+            type: 'broadcast',
+            payload: 'MeSsAgE',
+            event: 'msg',
+        })
+    }
 
     return(
         <div>
             {SessionID}
+            <button onClick={sendmsg}>Send</button>
         </div>
     )
 }
