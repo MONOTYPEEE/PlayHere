d90c24ff6870eeef4cc4905dcb10b6e352beca40
김상구 <spair0039@gmail.com>
2023-08-25 15:11:49 +0900

    ✨ 유튜브 검색 결과 타이핑

---
+++ src/types/YouTube.d.ts
@@ -0,0 +1,46 @@
+declare interface YouTubeThumbnail {
+    url: string;
+    width: number;
+    height: number;
+}
+
+declare interface YouTubeVideoID {
+    kind: string;
+    videoId: string;
+}
+
+declare interface YouTubeSnippet {
+    publishedAt: string;
+    channelId: string;
+    title: string;
+    description: string;
+    thumbnails: {
+        default: Thumbnail;
+        medium: Thumbnail;
+        high: Thumbnail;
+    };
+    channelTitle: string;
+    liveBroadcastContent: string;
+    publishTime: string;
+}
+
+declare interface YouTubeVideoItem {
+    kind: string;
+    etag: string;
+    id: YouTubeVideoID;
+    snippet: YouTubeSnippet;
+}
+
+declare interface YoutubeResponsePageInfo {
+    totalResults: number;
+    resultsPerPage: number;
+}
+
+declare interface YoutubeResponse {
+    kind: string;
+    etag: string;
+    nextPageToken: string;
+    regionCode: string;
+    pageInfo: YoutubeResponsePageInfo;
+    items: YouTubeVideoItem[];
+}
