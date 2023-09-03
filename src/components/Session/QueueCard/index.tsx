1eabf88aa7260d1de3783446054a958b57acb7e6
김상구 <spair0039@gmail.com>
2023-09-03 15:44:48 +0900

    🎨 대기열 표시(임시)

--- src/components/Session/QueueCard/index.tsx
+++ src/components/Session/QueueCard/index.tsx
@@ -1,9 +1,15 @@
 import { Card, Title2 } from "@fluentui/react-components";
 import { QueueCardStyle } from "./style"
 import QueueAddDialog from "../QueueAddDialog"
+import { useRecoilState } from "recoil";
+import { PlaylistQueue } from "@/atoms/PlaylistQueue";
+import VideoThumb from "../VideoThumb";
 
 export default function QueueCard(){
     const style = QueueCardStyle()
+    const [Queue,setQueue] = useRecoilState(PlaylistQueue)
+
+    
 
     return(
         <Card>
@@ -11,6 +17,9 @@ export default function QueueCard(){
                 <Title2>재생 대기열</Title2>
                 <QueueAddDialog/>
             </div>
+            {Queue.map((d)=>{
+                return <VideoThumb data={d} key={d.id.videoId}/>
+            })}
         </Card>
     )
 }
