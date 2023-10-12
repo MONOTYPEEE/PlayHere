92ad6371926086b3a44baf2ba40f021839f344dd
김상구 <spair0039@gmail.com>
2023-10-13 08:34:29 +0900

    ♻️ 미사용 import 및 코드 삭제

--- src/components/Session/QueueCard/index.tsx
+++ src/components/Session/QueueCard/index.tsx
@@ -2,15 +2,10 @@ import { Card, Title2 } from "@fluentui/react-components";
 import { QueueCardStyle } from "./style"
 import QueueAddDialog from "../QueueAddDialog"
 import { useRecoilState } from "recoil";
-import { PlaylistQueue } from "@/atoms/PlaylistQueue";
 import VideoThumb from "../VideoThumb";
-import { supabase } from "@/lib/supabaseInit";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 import { SessionTableAtom } from "@/atoms/SessionTableAtom";
 
 export default function QueueCard(){
-    const router = useRouter()
     const style = QueueCardStyle()
     const [SessionData, setSessionData] = useRecoilState(SessionTableAtom)
 
