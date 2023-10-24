dd059f3d17c537b6c241f1b4e4e82c8fe53cdff4
김상구 <spair0039@gmail.com>
2023-10-24 16:26:44 +0900

    🎨 VideoThumb undefined 핸들링

--- src/components/Session/VideoThumb/index.tsx
+++ src/components/Session/VideoThumb/index.tsx
@@ -2,19 +2,32 @@ import { Body1, Image, Subtitle1, Subtitle2 } from "@fluentui/react-components"
 import { VideoThumbStyle } from "./style"
 
 interface videoThumbProps {
-    data: YouTubeVideoItem
+    data: YouTubeVideoItem | undefined
 }
 
 export default function VideoThumb({data}:videoThumbProps){
     const style = VideoThumbStyle()
 
-    return(
-        <div className={style.main}>
-            <Image src={data.snippet.thumbnails.medium.url} width={160} height={90} shadow shape="rounded"/>
-            <div className={style.vertical}>
-                <Subtitle1 className={style.text}>{data.snippet.title}</Subtitle1>
-                <Body1>{data.snippet.channelTitle}</Body1>
+    if(data === undefined){
+        return(
+            <div className={style.main}>
+                <div className={style.thumbnailDummy}/>
+                <div className={style.vertical}>
+                    <Subtitle1 className={style.text}>미디어 없음</Subtitle1>
+                    <Body1>아티스트</Body1>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+    else{
+        return(
+            <div className={style.main}>
+                <Image src={data.snippet.thumbnails.medium.url} width={160} height={90} shadow shape="rounded"/>
+                <div className={style.vertical}>
+                    <Subtitle1 className={style.text}>{data.snippet.title}</Subtitle1>
+                    <Body1>{data.snippet.channelTitle}</Body1>
+                </div>
+            </div>
+        )
+    }
 }
