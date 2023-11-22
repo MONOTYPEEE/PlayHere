087f9d483fe03b2e53e39f5fa37d262ef1122304
김상구 <spair0039@gmail.com>
2023-11-22 13:30:59 +0900

    ✨ 로그아웃 다이얼로그 분리

--- src/components/Landing/HeaderUserProfile/index.tsx
+++ src/components/Landing/HeaderUserProfile/index.tsx
@@ -1,25 +1,18 @@
 import { IsLoggedinState } from "@/atoms/LoginAtom"
 import { supabase } from "@/util/supabaseInit"
-import { MenuButton, Menu, MenuItem, MenuList, MenuPopover, MenuTrigger, Persona } from "@fluentui/react-components"
+import { MenuButton, Menu, MenuItem, MenuList, MenuPopover, MenuTrigger, Persona, Portal } from "@fluentui/react-components"
 import { SignOut24Filled } from "@fluentui/react-icons"
 import { Session } from "@supabase/supabase-js"
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 import { useRecoilState } from "recoil"
+import SignOutDialog from "../SignOutDialog"
 
 export default function HeaderUserProfile(){
     const router = useRouter()
-    const [isLoggedIn,setLoggedIn] = useRecoilState(IsLoggedinState)
-    const [AccountData,setAccountData] = useState<Session>()
+    const [isDialogOpen,setDialogOpen] = useState<boolean>(false)
 
-    function signOutHandler(){
-        supabase.auth.signOut()
-        supabase.auth.onAuthStateChange((event, session)=>{
-            if(event === 'SIGNED_OUT'){
-                setLoggedIn(false)
-            }
-        })
-    }
+    const [AccountData,setAccountData] = useState<Session>()
 
     useEffect(()=>{
         const storedTokenData = localStorage.getItem('sb-grnaqmvtqsslvrivlqob-auth-token')
@@ -29,18 +22,23 @@ export default function HeaderUserProfile(){
     },[])
 
     return(
-        <Menu>
-            <MenuTrigger>
-                <MenuButton appearance="subtle" size="small">
-                    <Persona name={AccountData?.user.email} textAlignment="center" size="small"/>
-                </MenuButton>
-            </MenuTrigger>
+        <>
+            <Menu>
+                <MenuTrigger>
+                    <MenuButton appearance="subtle" size="small">
+                        <Persona name={AccountData?.user.email} textAlignment="center" size="small"/>
+                    </MenuButton>
+                </MenuTrigger>
 
-            <MenuPopover>
-                <MenuList>
-                    <MenuItem icon={<SignOut24Filled/>} onClick={signOutHandler}>로그아웃</MenuItem>
-                </MenuList>
-            </MenuPopover>
-        </Menu>
+                <MenuPopover>
+                    <MenuList>
+                        <MenuItem icon={<SignOut24Filled/>} onClick={()=>setDialogOpen(true)}>로그아웃</MenuItem>
+                    </MenuList>
+                </MenuPopover>
+            </Menu>
+            <Portal>
+                <SignOutDialog setState={setDialogOpen} state={isDialogOpen}/>
+            </Portal>
+        </>
     )
 }
